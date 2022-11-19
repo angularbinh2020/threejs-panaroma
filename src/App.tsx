b1a73ad647ad8e61fa781d550ed2485a0977ab57
doducbinh1995 <doducbinh1995@gmail.com>
2022-11-19 22:41:51 +0700

    init threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  PerspectiveCamera,
+  Scene,
+  SphereGeometry,
+  TextureLoader,
+  MeshBasicMaterial,
+  Mesh,
+  WebGLRenderer,
+  MathUtils,
+  BoxGeometry,
+  Texture,
+  ImageLoader,
+} from "three";
+//@ts-ignore
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const cameraRef = useRef<PerspectiveCamera>();
+  const sceneRef = useRef<Scene>();
+  const rendererRef = useRef<WebGLRenderer>();
+  const animateNumber = useRef<number>();
+  const controlsRef = useRef<OrbitControls>();
+  useEffect(() => {
+    function getTexturesFromAtlasFile(atlasImgUrl: string, tilesNum: number) {
+      const textures: any[] = [];
+
+      for (let i = 0; i < tilesNum; i++) {
+        textures[i] = new Texture();
+      }
+
+      new ImageLoader().load(atlasImgUrl, (image) => {
+        const tileWidth = image.width;
+
+        for (let i = 0; i < textures.length; i++) {
+          const canvas = document.createElement("canvas");
+          const context = canvas.getContext("2d");
+          canvas.height = tileWidth;
+          canvas.width = tileWidth;
+          context?.drawImage(
+            image,
+            0,
+            tileWidth * i,
+            tileWidth,
+            tileWidth,
+            0,
+            0,
+            tileWidth,
+            tileWidth
+          );
+          textures[i].image = canvas;
+          textures[i].needsUpdate = true;
+        }
+      });
+      const backImage = textures[0];
+      const downImage = textures[1];
+      const fontImage = textures[2];
+      const leftImage = textures[3];
+      const rightImage = textures[4];
+      const upImage = textures[5];
+      return [rightImage, leftImage, upImage, downImage, fontImage, backImage];
+    }
+    const containerElement = containerRef.current;
+    function onWindowResize() {
+      const camera = cameraRef.current;
+      const renderer = rendererRef.current;
+      if (camera && renderer) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      }
+    }
+
+    function animate() {
+      if (animateNumber.current) {
+        cancelAnimationFrame(animateNumber.current);
+      }
+      animateNumber.current = requestAnimationFrame(animate);
+      update();
+    }
+
+    function update() {
+      const camera = cameraRef.current;
+      const renderer = rendererRef.current;
+      const scene = sceneRef.current;
+      const controls = controlsRef.current;
+      if (camera && renderer && scene && controls) {
+        controls.update(); // required when damping is enabled
+        renderer.render(scene, camera);
+      }
+    }
+
+    function clearOldView() {
+      const camera = cameraRef.current;
+      const renderer = rendererRef.current;
+      const scene = sceneRef.current;
+      const controls = controlsRef.current;
+      if (camera && renderer && scene && controls && containerElement) {
+        scene.clear();
+        camera.clear();
+        renderer.dispose();
+        containerElement.innerHTML = "";
+      }
+    }
+
+    if (containerElement) {
+      if (rendererRef.current) {
+        clearOldView();
+      }
+      // const previewUrl =
+      //   "https://threejs.org/examples/textures/cube/sun_temple_stripe.jpg";
+      const previewUrl =
+        "https://nhathat.azureedge.net/vrdev360/394ad317-d5f3-440a-bcf3-b5b150e3c499/preview.jpg";
+      const containerWidth = containerElement.offsetWidth;
+      const containerHeight = containerElement.offsetHeight;
+      const camera = new PerspectiveCamera(
+        90,
+        containerWidth / containerHeight,
+        0.1,
+        100
+      );
+      const renderer = new WebGLRenderer();
+      rendererRef.current = renderer;
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(containerWidth, containerHeight);
+      containerElement.appendChild(renderer.domElement);
+      const scene = new Scene();
+      sceneRef.current = scene;
+      camera.position.z = 0.01;
+      cameraRef.current = camera;
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableZoom = false;
+      controls.enablePan = false;
+      controls.enableDamping = true;
+      controls.rotateSpeed = -0.25;
+      controlsRef.current = controls;
+      const textures = getTexturesFromAtlasFile(previewUrl, 6);
+
+      const materials = [];
+
+      for (let i = 0; i < 6; i++) {
+        materials.push(new MeshBasicMaterial({ map: textures[i] }));
+      }
+
+      const skyBox = new Mesh(new BoxGeometry(1, 1, 1), materials);
+      skyBox.geometry.scale(1, 1, -1);
+      scene.add(skyBox);
+      window.addEventListener("resize", onWindowResize);
+    }
+    animate();
+    return () => {
+      if (animateNumber.current) {
+        cancelAnimationFrame(animateNumber.current);
+      }
+      window.removeEventListener("resize", onWindowResize);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+      ref={containerRef}
+    ></div>
   );
 }
 
---
+++ src/Mock.ts
@@ -0,0 +1,403 @@
+export const TourSettingMock = {
+    imagesSetting: [
+      {
+        isCompletedSetup: true,
+        tagHotSpots: [
+          {
+            id: '394ad317-d5f3-440a-bcf3-b5b150e3c499-tag-Tue Aug 02 2022 11:49:51 GMT+0700 (Indochina Time)',
+            icon: {
+              IconCode: 5,
+              IconUrl: 'https://nhathat.azureedge.net/vrtours/Tag/Tag5.svg',
+            },
+            yaw: 1.286670793756409,
+            pitch: 0.7073051663379069,
+            title: 'GIƯỜNG',
+          },
+          {
+            id: '394ad317-d5f3-440a-bcf3-b5b150e3c499-tag-Tue Aug 02 2022 11:49:58 GMT+0700 (Indochina Time)',
+            icon: {
+              IconCode: 7,
+              IconUrl: 'https://nhathat.azureedge.net/vrtours/Tag/Tag7.svg',
+            },
+            yaw: 2.066144070391343,
+            pitch: 0.3321283682597489,
+            title: 'LỐI RA HÀNH LANG',
+          },
+        ],
+        nextMarkerPosition: {
+          id: '394ad317-d5f3-440a-bcf3-b5b150e3c499-NextMarker',
+          imageTarget: {
+            Id: '20c92af9-fe53-48f4-83f4-1305d61f5981',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118679181353_10- Loi Vao Phong Ngu 2.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119643095863_pk.png',
+            Order: 2,
+            Title: '10- Loi Vao Phong Ngu 2.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/20c92af9-fe53-48f4-83f4-1305d61f5981/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: 2.555827628202705,
+          pitch: 0.8508882938472091,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+          defaultView: { yaw: -0.19876330541448084, pitch: 0.006508578983858371 },
+        },
+        previousMarkerPosition: {
+          id: '394ad317-d5f3-440a-bcf3-b5b150e3c499-PreviousMarker',
+          imageTarget: {
+            Id: '6e0010da-8405-4c02-a369-a0aeac574dd2',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118672035571_8-Phong Ngu 1.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119698435489_hinh dai dien.jpg',
+            Order: 4,
+            Title: '8-Phong Ngu 1.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/6e0010da-8405-4c02-a369-a0aeac574dd2/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        defaultView: { yaw: 0.13784249609246757, pitch: 0.017129646500904983 },
+        previewImageSize: 280,
+        Id: '394ad317-d5f3-440a-bcf3-b5b150e3c499',
+        OriginalUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950118666741243_9- View Phong Ngu 1.jpg',
+        ThumbnailUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950119605187082_hinh dai dien.jpg',
+        Order: 1,
+        Title: '9- View Phong Ngu 1.jpg',
+        ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+        TitleImageUrl:
+          'https://nhathat.azureedge.net/vrdev360/394ad317-d5f3-440a-bcf3-b5b150e3c499/{f}_{x}_{y}.jpg',
+        TitleSize: 840,
+        imageFooterCoverSetting: {
+          isUsed: true,
+          yaw: 0,
+          pitch: 1.5707963267948966,
+          width: 420,
+          height: 420,
+          radius: 840,
+          id: 'footer-cover-id-1659415767971',
+        },
+        customHotSpots: [
+          {
+            id: '394ad317-d5f3-440a-bcf3-b5b150e3c499-custom-hotspot-Sat Oct 29 2022 21:30:44 GMT+0700 (Indochina Time)',
+            width: 40,
+            yaw: 2.208988200409605,
+            pitch: 0.5075364074976427,
+            defaultView: { yaw: 0.8503253019731005, pitch: 0.38767559802075624 },
+            imageTarget: {
+              Id: '6e0010da-8405-4c02-a369-a0aeac574dd2',
+              OriginalUrl:
+                'https://brokerdev.azureedge.net/virtualtour360/Test/637950118672035571_8-Phong Ngu 1.jpg',
+              ThumbnailUrl:
+                'https://brokerdev.azureedge.net/virtualtour360/Test/637950119698435489_hinh dai dien.jpg',
+              Order: 4,
+              Title: '8-Phong Ngu 1.jpg',
+              ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+              TitleImageUrl:
+                'https://nhathat.azureedge.net/vrdev360/6e0010da-8405-4c02-a369-a0aeac574dd2/{f}_{x}_{y}.jpg',
+              TitleSize: 840,
+            },
+          },
+        ],
+        miniMapSetting: {
+          left: 250,
+          top: 200,
+          centerX: 270,
+          centerY: 220,
+          id: 'image-id-394ad317-d5f3-440a-bcf3-b5b150e3c499-position-id-1668440469924',
+          miniMapId: 'd88a74a2-4f75-4a38-8764-c2a5fed0bc77',
+          northDeg: -0.3295638379972434,
+          touchRadius: 0,
+        },
+      },
+      {
+        isCompletedSetup: true,
+        tagHotSpots: [
+          {
+            id: '20c92af9-fe53-48f4-83f4-1305d61f5981-tag-Tue Aug 02 2022 11:50:22 GMT+0700 (Indochina Time)',
+            icon: {
+              IconCode: 12,
+              IconUrl: 'https://nhathat.azureedge.net/vrtours/Tag/Tag12.svg',
+            },
+            yaw: 3.1287442890190533,
+            pitch: 0.2774047731289091,
+            title: 'CỬA',
+          },
+          {
+            id: '20c92af9-fe53-48f4-83f4-1305d61f5981-tag-Tue Aug 02 2022 11:50:44 GMT+0700 (Indochina Time)',
+            icon: {
+              IconCode: 8,
+              IconUrl: 'https://nhathat.azureedge.net/vrtours/Tag/Tag8.svg',
+            },
+            yaw: 0.9855883261119622,
+            pitch: 0.43791354768409185,
+            title: 'CỬA RA VÀO',
+          },
+        ],
+        nextMarkerPosition: {
+          id: '20c92af9-fe53-48f4-83f4-1305d61f5981-NextMarker',
+          imageTarget: {
+            Id: '162711c8-4e3e-4e98-7773-d51a8674774b',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118680471359_7-Loi Vao Phong Ngu.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119676903341_wcphongkahc.png',
+            Order: 3,
+            Title: '7-Loi Vao Phong Ngu.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/162711c8-4e3e-4e98-7773-d51a8674774b/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: 1.0346005655857518,
+          pitch: 0.8514375226842539,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        previousMarkerPosition: {
+          id: '20c92af9-fe53-48f4-83f4-1305d61f5981-PreviousMarker',
+          imageTarget: {
+            Id: '394ad317-d5f3-440a-bcf3-b5b150e3c499',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118666741243_9- View Phong Ngu 1.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119605187082_hinh dai dien.jpg',
+            Order: 1,
+            Title: '9- View Phong Ngu 1.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/394ad317-d5f3-440a-bcf3-b5b150e3c499/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: 2.5106506914688342,
+          pitch: 0.694263646022959,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        defaultView: { yaw: 1.2880675284661578, pitch: 1.0284354165057543 },
+        previewImageSize: 280,
+        Id: '20c92af9-fe53-48f4-83f4-1305d61f5981',
+        OriginalUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950118679181353_10- Loi Vao Phong Ngu 2.jpg',
+        ThumbnailUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950119643095863_pk.png',
+        Order: 2,
+        Title: '10- Loi Vao Phong Ngu 2.jpg',
+        ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+        TitleImageUrl:
+          'https://nhathat.azureedge.net/vrdev360/20c92af9-fe53-48f4-83f4-1305d61f5981/{f}_{x}_{y}.jpg',
+        TitleSize: 840,
+        imageFooterCoverSetting: {
+          isUsed: true,
+          yaw: 0,
+          pitch: 1.5707963267948966,
+          width: 420,
+          height: 420,
+          radius: 840,
+          id: 'footer-cover-id-1659415833324',
+        },
+        customHotSpots: [],
+        miniMapSetting: {
+          left: 250,
+          top: 200,
+          centerX: 270,
+          centerY: 220,
+          id: 'image-id-20c92af9-fe53-48f4-83f4-1305d61f5981-position-id-1668440502296',
+          miniMapId: 'd88a74a2-4f75-4a38-8764-c2a5fed0bc77',
+          northDeg: 1.2880675284661578,
+          touchRadius: 0,
+        },
+      },
+      {
+        isCompletedSetup: true,
+        tagHotSpots: [
+          {
+            id: '162711c8-4e3e-4e98-7773-d51a8674774b-tag-Tue Aug 02 2022 11:51:06 GMT+0700 (Indochina Time)',
+            icon: {
+              IconCode: 7,
+              IconUrl: 'https://nhathat.azureedge.net/vrtours/Tag/Tag7.svg',
+            },
+            yaw: 0.1834848860104472,
+            pitch: -0.0163536614821993,
+            title: 'VIEW CỬA SỔ ',
+          },
+        ],
+        nextMarkerPosition: {
+          id: '162711c8-4e3e-4e98-7773-d51a8674774b-NextMarker',
+          imageTarget: {
+            Id: '6e0010da-8405-4c02-a369-a0aeac574dd2',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118672035571_8-Phong Ngu 1.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119698435489_hinh dai dien.jpg',
+            Order: 4,
+            Title: '8-Phong Ngu 1.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/6e0010da-8405-4c02-a369-a0aeac574dd2/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: 0.010114390953715358,
+          pitch: 0.7588090509318306,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        previousMarkerPosition: {
+          id: '162711c8-4e3e-4e98-7773-d51a8674774b-PreviousMarker',
+          imageTarget: {
+            Id: '20c92af9-fe53-48f4-83f4-1305d61f5981',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118679181353_10- Loi Vao Phong Ngu 2.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119643095863_pk.png',
+            Order: 2,
+            Title: '10- Loi Vao Phong Ngu 2.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/20c92af9-fe53-48f4-83f4-1305d61f5981/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: 0.04921139856794454,
+          pitch: 0.3927000660070217,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        defaultView: { yaw: 0.25408869899722575, pitch: 0.2696594086940127 },
+        previewImageSize: 280,
+        Id: '162711c8-4e3e-4e98-7773-d51a8674774b',
+        OriginalUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950118680471359_7-Loi Vao Phong Ngu.jpg',
+        ThumbnailUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950119676903341_wcphongkahc.png',
+        Order: 3,
+        Title: '7-Loi Vao Phong Ngu.jpg',
+        ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+        TitleImageUrl:
+          'https://nhathat.azureedge.net/vrdev360/162711c8-4e3e-4e98-7773-d51a8674774b/{f}_{x}_{y}.jpg',
+        TitleSize: 840,
+        imageFooterCoverSetting: {
+          isUsed: true,
+          yaw: 0,
+          pitch: 1.5707963267948966,
+          width: 420,
+          height: 420,
+          radius: 840,
+          id: 'footer-cover-id-1659415833329',
+        },
+        customHotSpots: [],
+        miniMapSetting: {
+          left: 119,
+          top: 156,
+          centerX: 139,
+          centerY: 176,
+          id: 'image-id-162711c8-4e3e-4e98-7773-d51a8674774b-position-id-1668440506183',
+          miniMapId: 'd88a74a2-4f75-4a38-8764-c2a5fed0bc77',
+          northDeg: 0.4030751473571321,
+          touchRadius: 0,
+        },
+      },
+      {
+        isCompletedSetup: true,
+        tagHotSpots: [],
+        nextMarkerPosition: {
+          id: '6e0010da-8405-4c02-a369-a0aeac574dd2-NextMarker',
+          imageTarget: {
+            Id: '394ad317-d5f3-440a-bcf3-b5b150e3c499',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118666741243_9- View Phong Ngu 1.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119605187082_hinh dai dien.jpg',
+            Order: 1,
+            Title: '9- View Phong Ngu 1.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/394ad317-d5f3-440a-bcf3-b5b150e3c499/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        previousMarkerPosition: {
+          id: '6e0010da-8405-4c02-a369-a0aeac574dd2-PreviousMarker',
+          imageTarget: {
+            Id: '162711c8-4e3e-4e98-7773-d51a8674774b',
+            OriginalUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950118680471359_7-Loi Vao Phong Ngu.jpg',
+            ThumbnailUrl:
+              'https://brokerdev.azureedge.net/virtualtour360/Test/637950119676903341_wcphongkahc.png',
+            Order: 3,
+            Title: '7-Loi Vao Phong Ngu.jpg',
+            ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+            TitleImageUrl:
+              'https://nhathat.azureedge.net/vrdev360/162711c8-4e3e-4e98-7773-d51a8674774b/{f}_{x}_{y}.jpg',
+            TitleSize: 840,
+          },
+          yaw: -0.20736820974447845,
+          pitch: 0.6116081596596246,
+          width: 40,
+          mobileScale: 100,
+          icon: {
+            IconCode: 1,
+            IconUrl: 'https://nhathat.azureedge.net/vrtours/hotspot/Mogivi01.svg',
+          },
+        },
+        defaultView: { yaw: 0, pitch: 0 },
+        previewImageSize: 280,
+        Id: '6e0010da-8405-4c02-a369-a0aeac574dd2',
+        OriginalUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950118672035571_8-Phong Ngu 1.jpg',
+        ThumbnailUrl:
+          'https://brokerdev.azureedge.net/virtualtour360/Test/637950119698435489_hinh dai dien.jpg',
+        Order: 4,
+        Title: '8-Phong Ngu 1.jpg',
+        ResolutionOriginalUrl: { Width: 6720, Height: 3360 },
+        TitleImageUrl:
+          'https://nhathat.azureedge.net/vrdev360/6e0010da-8405-4c02-a369-a0aeac574dd2/{f}_{x}_{y}.jpg',
+        TitleSize: 840,
+        customHotSpots: [],
+        miniMapSetting: {
+          left: 250,
+          top: 200,
+          centerX: 270,
+          centerY: 220,
+          id: 'image-id-6e0010da-8405-4c02-a369-a0aeac574dd2-position-id-1668440513422',
+          miniMapId: 'd88a74a2-4f75-4a38-8764-c2a5fed0bc77',
+          northDeg: 0,
+          touchRadius: 0,
+        },
+      },
+    ],
+  };
+  
